26043babbb94f5b70f53dd273d03115b20d4c398
kvrolinne <167714855+kvrolinne@users.noreply.github.com>
2024-04-22 17:51:12 -0300

    Exercício 4

--- Lista 1/Exer4.c
+++ Lista 1/Exer4.c
@@ -3,15 +3,14 @@
 //Exercício: Faça um algoritmo para ler um valor em quilogramas (Kg) e imprimir o equivalente em gramas (g).
 
 int main() {
-    float quilogramas;
-    float gramas;
+    float quilogramas, gramas;
 
     printf("Digite o valor em quilogramas (Kg): ");
     scanf("%f", &quilogramas);
 
     gramas = quilogramas * 1000;
 
-    printf("%.2f Kg equivale a %.2f gramas (g)\n", quilogramas, gramas);
+    printf("%.2f Kg equivale a %.0f gramas (g)\n", quilogramas, gramas);
 
     return 0;
 }
