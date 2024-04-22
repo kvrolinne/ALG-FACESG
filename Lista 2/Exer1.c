52e447d656e08032d0596858654b45a583f8abcd
kvrolinne <167714855+kvrolinne@users.noreply.github.com>
2024-04-22 14:18:39 -0300

    Update Exer1.c

--- Lista 2/Exer1.c
+++ Lista 2/Exer1.c
@@ -1,1 +1,28 @@
-T
+#include <stdio.h>
+
+int main() {
+    int anoNascimento, anoAtual, idade;
+    float peso, altura, imc;
+
+    printf("Digite o ano de nascimento: ");
+    scanf("%d", &anoNascimento);
+
+    printf("Digite o peso em kg: ");
+    scanf("%f", &peso);
+
+    printf("Digite a altura em metros: ");
+    scanf("%f", &altura);
+  
+    printf("Digite o ano atual: ");
+    scanf("%d", &anoAtual);
+
+    idade = anoAtual - anoNascimento;
+
+    imc = peso / (altura * altura);
+
+    printf("\nIdade: %d anos\n", idade);
+    printf("Peso: %.2f kg\n", peso);
+    printf("IMC: %.2f\n", imc);
+
+    return 0;
+}
