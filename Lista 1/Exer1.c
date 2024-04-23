5f2c98c4c4868a781daf3a05940085061fc82a0d
kvrolinne <167714855+kvrolinne@users.noreply.github.com>
2024-04-23 18:05:09 -0300

    Exercício 1

--- Lista 1/Exer1.c
+++ Lista 1/Exer1.c
@@ -3,17 +3,13 @@
 //Excercício: Faça um algoritmo para ler dois valores e imprimir a soma dos mesmos.
 
 int main() {
-    int num1, num2, soma;
+    int num1, num2;
     
     printf("Digite um número: ");
     scanf("%d", &num1);
-    
     printf("Digite mais um número: ");
     scanf("%d", &num2);
-    
-    soma = num1 + num2;
-    
-    printf("A soma dos números é: %d", soma);
+    printf("A soma dos números é: %d", num1+num2);
     
     return 0;
 }
