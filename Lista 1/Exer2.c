ee3ee138329b0a5e0176cdf565e7be1c69b1ef49
kvrolinne <167714855+kvrolinne@users.noreply.github.com>
2024-04-23 18:07:02 -0300

    Update Exer2.c

--- Lista 1/Exer2.c
+++ Lista 1/Exer2.c
@@ -3,17 +3,13 @@
 //Exercício: Faça um algoritmo para ler dois valores e imprimir a multiplicação dos mesmos.
 
 int main() {
-    int num1, num2, multi;
+    int num1, num2;
     
     printf("Digite um número: ");
     scanf("%d", &num1);
-    
     printf("Digite mais um número: ");
     scanf("%d", &num2);
-    
-    multi = num1 * num2;
-    
-    printf("A multiplicação dos números é: %d", multi);
+    printf("A multiplicação dos números é: %d", num1*num2);
     
     return 0;
 }
