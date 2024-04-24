070180a265fbb2c7eec285cc9ae9d02fc9f00b15
kvrolinne <167714855+kvrolinne@users.noreply.github.com>
2024-04-24 19:04:43 -0300

    Exercício 4

---
+++ Lista 2/Exer4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Exercício: Maçãs custam R$ 0,30 se forem compradas menos que uma dúzia e R$0,25 se forem
+compradas uma dúzia ou mais. Escreva um programa em pseudocódigo que leia o número
+de maçãs compradas, calcule e escreva na tela o valor total da compra */
+
+int main() {
+    int qnt;
+
+    printf("Informe a quantidade de macas compradas: ");
+    scanf("%d", &qnt);
+
+    if (qnt < 12) 
+        printf("Valor da compra: R$%.2f\n", qnt * 0.30);
+    else 
+        printf("Valor da compra: R$%.2f\n", qnt * 0.25);
+
+return 0;
+}
