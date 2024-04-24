ce2a0e048b27280cc27bfd327725df3f2d913868
kvrolinne <167714855+kvrolinne@users.noreply.github.com>
2024-04-24 18:12:07 -0300

    Exercício 1

--- Lista 2/Exer1.c
+++ Lista 2/Exer1.c
@@ -1,1 +1,19 @@
-T
+#include <stdio.h>
+
+//Exercício: Faça um programa em C para ler dois valores e mostrar “EM ORDEM” caso o primeiro seja menor que o segundo e “FORA DE ORDEM” no caso contrário.
+
+int main(){
+    int num1, num2;
+    
+    printf("Digite dois números separadamente: ");
+    scanf("%d %d", &num1, &num2);
+    
+    if (num1 < num2) {
+        printf("Os números estão em ordem.");
+    } 
+    else {
+        printf("Os números estão fora de ordem.");
+    }
+    
+    return 0;
+}
