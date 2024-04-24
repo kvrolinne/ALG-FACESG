3b39cafe7a37499fec481fd37444a44b0181967a
kvrolinne <167714855+kvrolinne@users.noreply.github.com>
2024-04-24 18:29:31 -0300

    Exercício 2

---
+++ Lista 2/Exer2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+//Exercício: Faça um programa em C para ler o peso e a idade do usuário e mostrar qual o risco de apresentar um problema cardíaco:
+//peso maior que 110 kg e idade acima de 38 anos: ALTO RISCO
+//peso menor que 110kg e idade acima de 38 anos: MÉDIO RISCO
+//peso maior que 110kg e idade abaixo ou igual a 38 anos: BAIXO RISCO
+//nenhuma das anteriores: BAIXíSSIMO RISCO.
+
+int main(){
+    int idade;
+    float peso, imc;
+    
+    printf("Informe a sua idade: ");
+    scanf("%d", &idade);
+    printf("Informe o seu peso: ");
+    scanf("%f", &peso);
+    
+    if (peso > 110.00 && idade > 30){
+        printf("Alto risco de apresentar um problema cardíaco.");
+    }
+    else if (peso < 110.00 && idade > 38){
+        printf("Médio risco de apresentar um problema cardíaco.");
+    }
+    else if (peso > 110.00 && idade <= 38){
+        printf("Baixo risco de apresentar um problema cardíaco.");
+    }
+    else {
+        printf("Baixíssimo risco de apresentar um problema cardíaco.");
+    }
+    
+    return 0;
+}
