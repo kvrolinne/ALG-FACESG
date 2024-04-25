9a40f927d61eeccf123823770d56c5192cf22253
kvrolinne <167714855+kvrolinne@users.noreply.github.com>
2024-04-25 14:28:43 -0300

    Exercício 2

---
+++ Lista 3/Exer2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+/* Exercício: Faça um programa que imprima os números de 100 a 1, nesta ordem.*/
+
+int main (){
+    int i=100;
+    
+    while (i >= 1){
+        printf("%d ", i);
+        i--;
+    }
+    
+  return 0;
+}
