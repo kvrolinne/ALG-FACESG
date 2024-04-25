6bb589692f8ec5c9223775cd13fdfc0d6bbeacf6
kvrolinne <167714855+kvrolinne@users.noreply.github.com>
2024-04-25 14:11:01 -0300

    Exercício 1

---
+++ Lista 3/Exer1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+/* Exercício: Faça um programa que imprima os números de 1 a 100.*/
+
+int main (){
+    int i=1;
+    
+    while (i <= 100){
+        printf("%d ", i);
+        i++;
+    }
+    
+  return 0;
+}
